88b013bf78d499d93a604000037c34701d896f4a
Student <example@example.com>
2019-12-12 08:06:13 -0800

    update notes

---
+++ lib/ruby_logic.rb
@@ -0,0 +1,10 @@
+class Sentence
+  def initialize(word_to_find, replace_with_word)
+    # @word_to_find = word_to_find
+
+  end
+
+  def word_replace()
+    # @initial_sentence[@word_to_find] = @replace_with_word
+
+  end
---
+++ spec/ruby_logic_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('ruby_logic')
+
+describe('#ruby_logic') do
+  it("replaces a desired word with a new one") do
+    # words = Sentence.new("cat", "dog")
+    # expect(words.word_replace).to(eq("I am walking my dog to the cathedral"))
+  end
