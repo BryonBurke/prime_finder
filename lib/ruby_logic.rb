aa6b91d4e0ee485e30198275ef0d2d290730e8fa
Student <example@example.com>
2019-12-12 16:19:40 -0800

    create a prime number finding app to return every prime between 1 and user input

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -1,10 +1,17 @@
-class Sentence
-  def initialize(word_to_find, replace_with_word)
-    # @word_to_find = word_to_find
-
+class Prime
+  def initialize(user_number)
+    @user_number = user_number
+    @result_array = [2,3,5,7]
   end
-
-  def word_replace()
-    # @initial_sentence[@word_to_find] = @replace_with_word
-
+  def prime_finder()
+    number_line = (2..@user_number)
+    number_line.each() do |number|
+      if (number % 2 === 0) | (number % 3 === 0) | (number % 5 === 0) | (number % 7 === 0)
+      elsif
+        @result_array.push(number)
+      end
+    end
+    puts @result_array
+    return @result_array
   end
+end
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -1,8 +1,9 @@
 require('rspec')
 require('ruby_logic')
 
-describe('#ruby_logic') do
-  it("replaces a desired word with a new one") do
-    # words = Sentence.new("cat", "dog")
-    # expect(words.word_replace).to(eq("I am walking my dog to the cathedral"))
+describe('#prime_finder') do
+  it("returns all prime numbers between 1 and user input that are prime") do
+    numbers = Prime.new(50)
+    expect(numbers.prime_finder).to(eq([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]))
   end
+end
