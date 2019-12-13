f605a68df0376d732557dd2823a8f0abb8aff550
Student <example@example.com>
2019-12-12 16:22:23 -0800

    output list of primes from 2 up to user number

--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -3,7 +3,7 @@ require('ruby_logic')
 
 describe('#prime_finder') do
   it("returns all prime numbers between 1 and user input that are prime") do
-    numbers = Prime.new(50)
+    numbers = Prime.new(100)
     expect(numbers.prime_finder).to(eq([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]))
   end
 end
